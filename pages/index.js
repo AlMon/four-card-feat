f6430eb3854b8bae6ed14fd3f344b783a891cb7a
Al Mondragón <al.drago@live.com>
2021-07-25 19:19:26 -0500

    Finish mobile design

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,27 @@ import Card from '../components/Card'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[#fafafa] min-h-screen min-w-full'>
       <Head>
         <title>Four Card Feat</title>
         <meta name='description' content='Four Card Feat' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main
-        className='bg-[#fafafa] min-h-screen'
-        style={{ fontFamily: 'Poppins' }}
-      >
+      <main className='grid xl:default' style={{ fontFamily: 'Poppins' }}>
         <div className='text-center'>
           <p className='text-[#4c4e61] mt-10 text-4xl font-extralight leading-[3rem]'>
             Reliable, efficient delivery
             <br />
             <span className='font-semibold'>Powered by Technology</span>
           </p>
-          <p className='text-[#a3a5ae] mt-3 mx-[25rem]'>
+          <p className='text-[#a3a5ae] mt-3 mx-3 xl:mx-[25rem]'>
             Our Artificial Intelligence powered tools use millions of project
             data points to ensure that your project is successful
           </p>
         </div>
-        <div className='grid grid-cols-3 justify-center mt-14'>
-          <div className='ml-20 mt-28'>
+        <div className='grid grid-cols-1 xl:grid-cols-3 justify-center mt-14 mx-auto'>
+          <div className='xl:ml-20 xl:mt-28'>
             <Card
               Title='Supervisor'
               Copy='Monitors activity to identify project roadblocks'
@@ -34,7 +31,7 @@ export default function Home() {
               BorderColor='border-[#45d3d3]'
             />
           </div>
-          <div>
+          <div className='xl:mx-10'>
             <Card
               Title='Team Builder'
               Copy='Scans our talent network to create the optimal team for your project'
@@ -48,7 +45,7 @@ export default function Home() {
               BorderColor='border-[#fcaf4a]'
             />
           </div>
-          <div className='mt-28' style={{ marginLeft: '-5rem' }}>
+          <div className='xl:mt-28'>
             <Card
               Title='Calculator'
               Copy='Uses data from past projects to provide better delivery estimates'
